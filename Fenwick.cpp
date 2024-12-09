3341b195ee89dd86e81248d7e6c1dc7075a8508b
Yashasvi Chandra <146069599+Yasha5vi@users.noreply.github.com>
2024-12-09 12:18:42 +0530

    Update Fenwick.cpp

--- Fenwick.cpp
+++ Fenwick.cpp
@@ -1,30 +1,30 @@
-class FenwickTree {
-private:
-    vector<ll> fen; 
-    ll n; 
-public:
-    FenwickTree(int size) {
-        n = size;
-        memset(fen, 0, sizeof(fen));
-    }
 
-    void update(int idx, ll val) {
+class FenwickTree {
+public:
+    vector<ll> fen; ll n; 
+    FenwickTree(ll size) {
+        n = size;
+        fen.assign(n+1,0);;
+    }
+    void build(vector<ll>& arr) {
+        for (ll i = 0; i < n; i++) {
+            update(i + 1, arr[i]);
+        }
+    }
+    void update(ll idx, ll val) {
         while (idx <= n) {
             fen[idx] += val;
-            idx += (idx & (-idx)); 
+            idx = idx+(idx&(-idx)); 
         }
     }
-
-    ll sum(int idx) {
-        ll sum = 0;
+    ll sum(ll idx) {
+        ll res = 0;
         while (idx > 0) {
             res += fen[idx];
-            idx -= (idx & (-idx)); 
-        }
-        return res;
+            idx = idx-(idx&(-idx)); 
+        }return res;
     }
-
-    ll rangeSum(int left, int right) {
+    ll rangeSum(ll left,ll right) {
         return sum(right) - sum(left - 1);
     }
 };
