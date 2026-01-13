e509cb13be58dbac23fcfc392a2fdcc69bbce439
Yashasvi Chandra <yashasvichandra84@gmail.com>
2026-01-13 11:01:26 +0530

    Refactor nCr calculation using static array

--- nCr.cpp
+++ nCr.cpp
@@ -5,17 +5,13 @@ for(int i=2;i<=n;i++){
     fact[i] = ((i%M)*(fact[i-1]%M))%M;
 }
 
-vector<vector<long long>> ncr(n+1,vector<long long>(101));
-ncr[0][0] = 1;
-for(int i=1;i<=n;i++){
-    ncr[i][0] = 1;
-    if(i<=k){
-        ncr[0][i] = 0;
-    }
-}
-// nCr = (n-1)Cr + (n-1)C(r-1)
-for(int i=1;i<=n;i++){
-    for(int j=1;j<=min(100,i);j++){
-        ncr[i][j] = (ncr[i-1][j]%M + ncr[i-1][j-1]%M)%M;
+long long C[1001][1001]; 
+
+void build_nCr(int n) {
+    for (int i = 0; i <= n; i++) {
+        C[i][0] = C[i][i] = 1;
+        for (int j = 1; j < i; j++) {
+            C[i][j] = C[i-1][j] + C[i-1][j-1];
+        }
     }
 }
